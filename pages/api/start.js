f664683f7527f0511f45fa1b8d883323b562b048
Yevhen <sonyxperiasp90@gmail.com>
2020-09-27 14:56:23 +0300

    Sorting ADDED

--- pages/api/start.js
+++ pages/api/start.js
@@ -1,10 +1,12 @@
 import dbConnect from "../../utils/dbConnect";
+import Login from '../../models/Login'
 
 dbConnect()
 
 export default async function Start(req,res)
 {
     if(req.method==='GET'){
+        const temp = await Login.findOne({})
      return   res.status(201).json({data:'ready'})
     }
 
